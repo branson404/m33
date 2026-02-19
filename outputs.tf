7816cbb931f72bc0c0d7305834208ff72a7eb142
hp <shafi15112007@gmail.com>
2026-02-19 16:36:03 +0000

    address the module problem of calling things

--- outputs.tf
+++ outputs.tf
@@ -6,9 +6,9 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 output "vpc_id" {
-  value = module.vpc.aws_vpc.this.id
+  value = module.vpc.vpc_id
 }
 
 output "private_subnet_ids" {
-  value = values(module.vpc.aws_subnet.private)[*].id
+  value = module.vpc.private_subnet_ids
 }
