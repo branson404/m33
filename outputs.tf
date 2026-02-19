cf6595b110787df2aced9aaa45b0264d204b942d
Mohammed <78994175+branson404@users.noreply.github.com>
2026-02-19 16:20:17 +0000

    also added vpc outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,10 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "private_subnet_ids" {
+  value = values(aws_subnet.private)[*].id
+}
