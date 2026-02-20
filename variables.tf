c422580edfc652ca2af7caf9c7851bcb0043da29
Mohammed <78994175+branson404@users.noreply.github.com>
2026-02-20 09:12:04 +0000

    added RDS variables on main variables.tf

--- variables.tf
+++ variables.tf
@@ -22,14 +22,39 @@ variable "instance_types" {
   type = list(string)
 }
 
-variable "desired_size" {
-  type = number
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
 }
-
-variable "min_size" {
-  type = number
+variable "vpc_id" {type = string}
+variable "subnet_ids" {type  = list(string)}
+variable "allowed_cidr_blocks" {type = list(string)}
+variable "allocated_storage" {
+  type        = number
+  default     = 20
 }
-
-variable "max_size" {
-  type = number
+variable "engine" {
+  type        = string
+  default     = "mysql"
+}
+variable "engine_version" {
+  type        = string
+  default     = "8.0"
+}
+variable "instance_class" {
+  type        = string
+  default     = "db.t3.micro"
+}
+variable "db_name" {type = string}
+variable "db_username" {type = string}
+variable "db_password" {
+  type        = string
+}
+variable "db_port" {
+  type        = number
+  default     = 3306
+}
+variable "multi_az" {
+  type        = bool
+  default     = false
 }
