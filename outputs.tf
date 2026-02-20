0da75598cd7a12ba803b24688c31d5676dad6874
Mohammed <78994175+branson404@users.noreply.github.com>
2026-02-20 11:57:57 +0000

    added outputs of RDS

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "vpc_id" {
 output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
+
+output "rds_endpoint" {
+  value       = module.rds.aws_db_instance.rds.endpoint
+}
+
+output "rds_instance_id" {
+  value       = module.rds.aws_db_instance.rds.id
+}
+
+output "rds_security_group_id" {
+  value       = aws_security_group.rds_sg.id
+}
